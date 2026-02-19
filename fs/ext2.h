2eb01acd9b311401497215c169b306d2bbefd3d3
haxted <vlorpprolpor@gmail.com>
2026-02-19 14:54:20 +0200

    full ext2 structures (?)

--- fs/ext2.h
+++ fs/ext2.h
@@ -88,4 +88,94 @@ enum {
     EXT2_ROFEATURE_DIRCONTENTS_BT       = 0x4    
 };
 
-// TODO: everything else
+
+typedef struct {
+    uint32_t    blockUsageBmapAddr;
+    uint32_t    inodeUsageBmapAddr;
+    uint32_t    inodeTableAddr;
+    uint16_t    unallocatedBlocks;
+    uint16_t    unallocatedINodes;
+    uint16_t    numDirectories;
+} __attribute__((packed)) Ext2_BlockGroupDescriptor;
+
+typedef struct {
+    uint16_t    typePerms;
+    uint16_t    uid;
+    uint32_t    lowerSize;
+    uint32_t    lastAccessTime;
+    uint32_t    creationTime;
+    uint32_t    lastModificationTime;
+    uint32_t    deletionTime;
+    uint16_t    gid;
+    uint16_t    hardLinkCount;
+    uint32_t    diskSectorsUsed;
+    uint32_t    flags;
+    uint32_t    osSpecific1;
+    uint32_t    directPtrs[12];
+    uint32_t    singlyIndirectBPtr;
+    uint32_t    doublyIndirectBPtr;
+    uint32_t    triplyIndirectBPtr;
+    uint32_t    genNumber;
+    uint32_t    fileACL;
+    uint32_t    directoryACL;
+    uint32_t    fragmentBlockAddr;
+    char        osSpecific2[12];
+} __attribute__((packed)) Ext2_INode;
+
+enum {
+    EXT2_INODE_TYPE_FIFO = 0x1000,
+    EXT2_INODE_TYPE_CHARDEV = 0x2000,
+    EXT2_INODE_TYPE_DIRECTORY = 0x4000,
+    EXT2_INODE_TYPE_BLOCKDEV = 0x6000,
+    EXT2_INODE_TYPE_FILE = 0x8000,
+    EXT2_INODE_TYPE_SYMLINK = 0xA000,
+    EXT2_INODE_TYPE_USOCK = 0xC000
+};
+
+enum {
+    EXT2_INODE_PERM_OTHER_EXEC = 0x1,
+    EXT2_INODE_PERM_OTHER_WRITE = 0x2,
+    EXT2_INODE_PERM_OTHER_READ = 0x4,
+    EXT2_INODE_PERM_GROUP_EXEC = 0x8,
+    EXT2_INODE_PERM_GROUP_WRITE = 0x10,
+    EXT2_INODE_PERM_GROUP_READ = 0x20,
+    EXT2_INODE_PERM_USER_EXEC = 0x40,
+    EXT2_INODE_PERM_USER_WRITE = 0x80,
+    EXT2_INODE_PERM_USER_READ = 0x100,
+    EXT2_INODE_PERM_STICKY = 0x200,
+    EXT2_INODE_PERM_SETGID = 0x400,
+    EXT2_INODE_PERM_SETUID = 0x800
+};
+
+enum {
+    EXT2_INODE_FLAG_SECDEL = 0x1,
+    EXT2_INODE_FLAG_COPYWHENDEL = 0x2,
+    EXT2_INODE_FLAG_COMPRESS = 0x4,
+    EXT2_INODE_FLAG_SYNC = 0x8,
+    EXT2_INODE_FLAG_IMMUTABLE = 0x10,
+    EXT2_INODE_FLAG_APPEND = 0x20,
+    EXT2_INODE_FLAG_NODUMP = 0x40,
+    EXT2_INODE_FLAG_NOACCESSUPDATE = 0x80,
+    EXT2_INODE_FLAG_HASH = 0x10000,
+    EXT2_INODE_FLAG_AFS = 0x20000,
+    EXT2_INODE_FLAG_JOURNAL = 0x40000
+};
+
+typedef struct {
+    uint32_t    iNode;
+    uint16_t    totalSize;
+    uint8_t     nameLength;
+    uint8_t     typeOrMS8B;
+    // rest is name
+} __attribute__((packed)) Ext2_DirEnt;
+
+enum {
+    EXT2_DIRENT_TYPE_UNKNOWN,
+    EXT2_DIRENT_TYPE_REGULAR,
+    EXT2_DIRENT_TYPE_DIRECTORY,
+    EXT2_DIRENT_TYPE_CHARDEV,
+    EXT2_DIRENT_TYPE_BLOCKDEV,
+    EXT2_DIRENT_TYPE_FIFO,
+    EXT2_DIRENT_TYPE_SOCKET,
+    EXT2_DIRENT_TYPE_SYMLINK
+};
